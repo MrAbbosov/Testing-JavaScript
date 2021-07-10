65bccd90ece37a90ec20acba28b654ecddf990c1
MrAbbosov <farruh183@gmail.com>
2021-07-10 11:44:06 +0500

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const {sumAsync, subtractAsync} = require('./math.js')
+
+test("Sum numbers", async () => {
+    result = await sumAsync(2, 7)
+    expected = await 10
+    await expect(result).toBe(expected)
+})
+    
+async function test(title, callback) {
+    try{
+        await callback()
+        console.log(`ok ${title}`)
+    } catch(error){
+        console.error(`X ${title}`)
+        console.error(error)
+    }
+}
+
+function expect(actual) {
+    return {
+        toBe(expected){
+            if(actual !== expected) {
+                throw new Error (`${result} is not equal to ${expected}`)
+            }
+        }     
+    }
+}
